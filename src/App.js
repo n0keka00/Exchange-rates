8f2a446989ce7823c262c3401a886a44b04147f4
Katja Kettunen <n0keka00@students.oamk.fi>
2021-10-08 14:51:17 +0300

    Muokattu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
 import {useState} from 'react';
+import './App.css';
 
-const URL = 'http://api.exchangeratesapi.io/v1/'
+const URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=';
 const API_KEY = '1fb92ae77c8c88bead94a23d5513a274'
 
 
@@ -11,27 +9,6 @@ function App() {
   const [eur,setEur] = useState(0);
   const [gbp,setGbp] = useState(0);
   const [rate,setRate] = useState(0);
-
-  return (
-    <div id="container">
-      <form onSubmit={convert}>
-        <div>
-          <h1>Exchange rates</h1>
-          <label>Eur </label>&nbsp;
-          <input type="number" step="0.01"
-          value={eur} onChange={e => setEur(e.target.value)}/>
-          <output>{rate}</output>
-        </div>
-        <div>
-          <label>Gbp </label>
-          <output>{gbp.toFixed(2)} €</output>
-        </div>
-        <div>
-          <button>Calculate</button>
-        </div>
-      </form>
-    </div>
-  );
 
   async function convert(e) {
     e.preventDefault();
@@ -54,9 +31,27 @@ function App() {
     }
   }
 
-}
+  return (
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          <h1>Exchange rates</h1>
+          <label>Eur </label>&nbsp;
+          <input type="number" step="0.01"
+          value={eur} onChange={e => setEur(e.target.value)}/>
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>Gbp </label>
+          <output>{gbp.toFixed(2)} €</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
+    </div>
+  );
 
-
-
+  }
 
 export default App;
